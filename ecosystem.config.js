64ddc0d08b5a45a0b2e01e12c25dba6ef95dd944
tom <tom@tbrett.co.uk>
2024-11-11 22:09:00 +0000

    eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    apps: [
-      {
-        name: 'clips',
-        script: 'node_modules/.bin/next',
-        args: 'start -p 3001',
-        env: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'clips',
+      script: 'node_modules/next/dist/bin/next',
+      interpreter: 'node',
+      args: 'start -p 3001',
+      env: {
+        NODE_ENV: 'production',
       },
-    ],
-  };
-  
+    },
+  ],
+};
